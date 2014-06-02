957c7095436d3b0ac484ab5072fea4fd38326a3d
Timmay24 <tim.hartig@haw-hamburg.de>
2014-06-02 19:48:33 +0200

    Added Exception Types

---
+++ Aufgabe_3/src/merlin/logic/exception/InvalidConnectionDataException.java
@@ -0,0 +1,9 @@
+package merlin.logic.exception;
+
+public final class InvalidConnectionDataException extends BaseException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1024462394485028641L;
+}
