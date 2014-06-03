630da00d75450274abb166c1c92e77cc3a85ae3e
Timmay24 <tim.hartig@haw-hamburg.de>
2014-06-03 19:03:16 +0200

    Implemented accessors for login data for database user and birdwatcher. changed the way of encryption for login datas.
    Resetted config.properties for now.

--- Aufgabe_3/src/merlin/utils/ConstantElems.java
+++ Aufgabe_3/src/merlin/utils/ConstantElems.java
@@ -4,15 +4,20 @@ import java.io.File;
 import java.util.Properties;
 
 public final class ConstantElems {
-	public static final String defaultDbURL = "oracle.informatik.haw-hamburg.de";
-	public static final String defaultDbPort = "1521";
-	public static final String defaultDbSID = "inf09";
+	public static final String 	defaultDbURL = "oracle.informatik.haw-hamburg.de";
+	public static final String 	defaultDbPort = "1521";
+	public static final String 	defaultDbSID = "inf09";
 	
-	public static final String aKennungPropKey = "ak";
-	public static final String passwordPropKey  = "pd";
-	
-	public static Properties properties   = new Properties();
-	public static String     propFilePath = new File("").getAbsolutePath()
+	/* Properties Key Constants */
+	public static final String 	loginDataPropKey 			= "LD"; // value encrypted!
+	public static final String 	loginDataBirdwatcherPropKey = "BW"; // value encrypted!
+	public static final String 	loginDataSplitString 		= "\0";
+	public static final String 	dbURLPropKey				= "dbURL";
+	public static final String 	dbPortPropKey				= "dbPort";
+	public static final String 	dbSIDPropKey				= "dbSID";
+		
+	public static Properties 	properties   = new Properties();
+	public static String     	propFilePath = new File("").getAbsolutePath()
 									 + System.getProperty("file.separator")
 									 + "config.properties";
 	public static File propFile = new File(propFilePath);
